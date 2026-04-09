ec2a2099478b34de6cba4e27a20dc4fa1db627d9
LBoyles7 <lgboyles@noctrl.edu>
2026-04-09 11:31:54 -0500

    Add files via upload

---
+++ cipher.h
@@ -0,0 +1,32 @@
+#ifndef CIPHER_H
+#define CIPHER_H
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+string encryptString(string s) {
+	string new_string;
+	int index;
+	char keys[62] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 
+		'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 
+		'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 
+		'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+	for (char c:s) {
+		for (int count = 0; count < sizeof(keys); count++) {
+			if (c == keys[count]) {
+				index = (count + 3) % 62;
+				continue;
+			}
+		}
+		new_string += keys[index];
+
+	}
+
+	return new_string;
+
+}
+
+#endif
